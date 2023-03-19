dafaaf42a178e12c5d7d50f357a598a00c6bbcf5
jafir <tawfikjfr@gmail.com>
2023-03-19 10:48:12 -0700

    tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - pricipal function
+ * main - Entry of the program
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
