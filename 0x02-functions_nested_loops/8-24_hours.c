45a84dc2db7c5b97c902796b5b94e0e38656e61e
jafir <tawfikjfr@gmail.com>
2023-03-21 16:55:55 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void){
+	int i,j;
+	for ( i = 00 , i <= 24 , i++ ){
+		{
+			for (b = 0 ; b <= 59 ; b++)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+		}}
+	}
+
