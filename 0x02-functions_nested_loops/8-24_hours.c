622c8eac12ab4775dd16014355943c81f5859c23
jafir <tawfikjfr@gmail.com>
2023-03-21 17:04:29 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * jack_bauer - prints every minute of /**
+ *  * jack_bauer - prints every minute of the day of Jack Bauer
+ *   * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
@@ -24,5 +24,4 @@ void jack_bauer(void)
 			j++;
 		}
 		i++;
-	}
-}
+	}}
