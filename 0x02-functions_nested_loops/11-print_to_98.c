7d1d0997c2261cf603335b28413ee481225a9d21
jafir <tawfikjfr@gmail.com>
2023-03-21 17:34:35 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,16 +6,21 @@
  * followed by a new line
  * @n: print from this number
  */
-void print_to_98(int n){
-	int i,j;
-	if (n<=i){
-		for ( i=n ; i <= 98 ; i++ ){
-                        if (i != 98)
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
 				printf("%d, ", i);
 			else if (i == 98)
-				printf("%d\n", i);}}
-	else if (n >= 98)
-		{
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
